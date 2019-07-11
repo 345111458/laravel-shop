3838aa0248a6e84f3354bb1f41bbbcfaae3f7fbf
vagrant <vagrant@homestead>
2019-07-11 07:49:25 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Auth::routes();
 Auth::routes(['verify' => true]);
 
 
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
+
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
@@ -37,6 +41,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::redirect('/', '/products')->name('root');
 	Route::get('products', 'ProductsController@index')->name('products.index');
+
+
+
 });
 
 
