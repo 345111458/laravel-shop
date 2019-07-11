1abb4bef7959122c4f04d10825dbc155f87621e8
vagrant <vagrant@homestead>
2019-07-11 09:31:58 +0000

    商品加入购物车

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,10 +45,16 @@ class User extends Authenticatable implements MustVerifyEmail
 
 
     public function favoriteProducts(){
-        
+
         return $this->belongsToMany(Product::class, 'user_favorite_products')
             ->withTimestamps()
             ->orderBy('user_favorite_products.created_at', 'desc');
+    }
+
+
+    public function cartItems(){
+        
+        return $this->hasMany(CartItem::class);
     }
 
 
